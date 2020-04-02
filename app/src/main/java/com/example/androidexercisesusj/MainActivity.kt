33ae5dc0ed520b7c8c1746d828648a48fda435ba
Marcos Caballero <m.caballeroyus@gmail.com>
2020-04-02 08:34:41 +0200

    Finished Exercise

--- app/src/main/java/com/example/androidexercisesusj/MainActivity.kt
+++ app/src/main/java/com/example/androidexercisesusj/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.example.androidexercisesusj
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
+    var count=0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnChangeNumber: Button = findViewById(R.id.btn_change_integer)
+        btnChangeNumber.setOnClickListener {
+            Toast.makeText(this, "Changing number", Toast.LENGTH_SHORT).show()
+            count++
+            val textView:TextView = findViewById(R.id.text_counter)
+            textView.setText("It´s "+count)
+        }
     }
 }
