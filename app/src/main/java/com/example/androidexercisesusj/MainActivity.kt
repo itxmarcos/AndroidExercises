5c595f999c307cbdc1b4cdb0392f0bc596e4dac3
Marcos Caballero <m.caballeroyus@gmail.com>
2020-04-06 14:39:50 +0200

    Finished Exercise

--- app/src/main/java/com/example/androidexercisesusj/MainActivity.kt
+++ app/src/main/java/com/example/androidexercisesusj/MainActivity.kt
@@ -1,12 +1,49 @@
 package com.example.androidexercisesusj
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
+    private val TAG="MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.i(TAG, "onCreate")
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i(TAG, "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i(TAG, "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i(TAG, "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i(TAG, "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i(TAG, "onDestroy")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i(TAG, "onRestart")
+    }
+
 }
