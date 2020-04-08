d9b314af673fac29a339da76a0f7bed4d69b93dd
Marcos Caballero <m.caballeroyus@gmail.com>
2020-04-08 20:12:02 +0200

    Added Activities with their views in the XML files

---
+++ app/src/main/java/com/example/androidexercisesusj/ActivityC.kt
@@ -0,0 +1,12 @@
+package com.example.androidexercisesusj
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityC : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_c)
+    }
+}
---
+++ app/src/main/java/com/example/androidexercisesusj/ActivityD.kt
@@ -0,0 +1,12 @@
+package com.example.androidexercisesusj
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityD : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_d)
+    }
+}
