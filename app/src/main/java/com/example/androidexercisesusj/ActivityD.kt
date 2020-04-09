f0dc9d6c0a65678265a1b07dc4c81b485fe66f2f
Marcos Caballero <m.caballeroyus@gmail.com>
2020-04-09 15:46:56 +0200

    Activity D completed but wrong

--- app/src/main/java/com/example/androidexercisesusj/ActivityD.kt
+++ app/src/main/java/com/example/androidexercisesusj/ActivityD.kt
@@ -2,10 +2,17 @@ package com.example.androidexercisesusj
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.CheckBox
 import android.widget.TextView
 
 class ActivityD : AppCompatActivity() {
     val txtName = findViewById<TextView>(R.id.txt_name)
+    val txtWeight=findViewById<TextView>(R.id.txt_weight)
+
+    var weight1=0
+    var weight2=0
+    var weight3=0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,6 +22,29 @@ class ActivityD : AppCompatActivity() {
         val name=intent.getStringExtra("Name")
         txtName.text="Name: "+name //insert data into TextView
 
+        txtWeight.text="Weight: "+(weight1+weight2+weight3)+"kg"
+    }
 
+    fun onCheckboxClicked(view: View) {
+        if (view is CheckBox) {
+            val checked: Boolean = view.isChecked
+
+            when (view.id) {
+                R.id.checkBox1->
+                    if (checked) {
+                        weight1=1
+                    }
+
+                R.id.checkBox2 ->
+                    if (checked) {
+                        weight2=2
+                    }
+
+                R.id.checkBox3->
+                    if(checked){
+                        weight3
+                    }
+            }
+        }
     }
 }
